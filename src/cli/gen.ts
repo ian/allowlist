85107236b70693777c3b59c6829ac36b18d55607
Ian Hunter <ian@users.noreply.github.com>
2023-03-30 13:16:35 -0500

    better output for each file

--- src/cli/gen.ts
+++ src/cli/gen.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { Wallet, ethers } from "ethers";
 import { readFile, writeCSV, writeJSON } from "./config";
-import { Entry, Signature, SignatureWithSigner } from "types";
+import { Entry, SignatureWithSigner } from "types";
 import { createSignature } from "utils/sigs";
 
 export async function gen({
@@ -15,14 +15,18 @@ export async function gen({
   outputFile: string;
 }) {
   let count = 0;
+  const signers: { [file: string]: string } = {};
   const entries: { [addy: string]: Entry & { count: number } } = {};
   const errors: { address: string; error: string }[] = [];
   // const data = files.flatMap((file) => readFile(file));
 
   for (let i = 0; i < files.length; i++) {
     const data = readFile(files[i]);
-    const path = `m/44'/0'/0'/0/${i}`;
-    const generated = await genSigs(data, mnemonic, path);
+    const path = `m/44'/60'/0'/0/${i}`;
+    const signer = await getSigner(mnemonic, path);
+    const generated = await genSigs(data, signer);
+
+    signers[files[i]] = signer.address;
 
     generated.entries.map((rec) => {
       entries[rec.address] ||= {
@@ -32,55 +36,15 @@ export async function gen({
 
       // Always take the higher allocation
       if (rec.num > entries[rec.address].num) {
-        // entries[rec.address] ||= {
-        //   address: rec.address,
-        //   count: 0,
-        //   num: rec.num,
-        //   sig: rec.sig,
-        // };
         Object.assign(entries[rec.address], rec);
       }
 
       entries[rec.address].count += 1;
       count += 1;
-
-      // rec.num > (entries[rec.address]?.num || 0);
-      // entries[rec.address] ||= {
-      //   address: rec.address,
-      //   count: 0,
-      //   num: rec.num,
-      //   sig: rec.sig,
-      // };
-      // entries[address].count += 1;
     });
 
     errors.concat(errors);
   }
-
-  // for (const a of data) {
-  //   const addy =
-  //     a.address || // basic
-  //     a.wallet || // basic
-  //     a.mint_wallet_address; // premint
-
-  //   const allocation = a.allocation || 1;
-
-  //   if (!addy || addy === "") continue;
-  //   try {
-  //     const address = ethers.utils.getAddress(addy);
-  //     entries[address] ||= {
-  //       address: addy,
-  //       count: 0,
-  //       allocation: parseInt(allocation),
-  //     };
-  //     entries[address].count += 1;
-  //   } catch (err) {
-  //     errors.push({
-  //       address: addy,
-  //       error: err.message,
-  //     });
-  //   }
-  // }
 
   const deduped = Object.values(entries);
   const dupes = Object.entries(entries).filter(
@@ -95,10 +59,6 @@ export async function gen({
   console.log("Dupes: ", dupes.length);
 
   const signatures = {};
-
-  // // eventually we'll make this iterative over the list of mint groups
-  // const keyPath = "m/44'/0'/0'";
-  // const signer = await getSigner(mnemonic, keyPath);
 
   for (const entry of deduped) {
     const { address, sig } = entry;
@@ -117,12 +77,19 @@ export async function gen({
     console.log("Writing", errors.length, "errors to errors.csv");
     await writeCSV(process.cwd() + "/errors.csv", errors);
   }
+
+  Object.entries(signers).map(([file, address]) => {
+    console.log();
+    console.log("File:  ", file);
+    console.log("Signer:", address);
+  });
 }
 
 async function genSigs(
   data: { [key: string]: any }[],
-  mnemonic: string,
-  keyPath: string
+  signer: Signer
+  // mnemonic: string,
+  // keyPath: string
 ) {
   const entries: {
     address: string;
@@ -131,7 +98,6 @@ async function genSigs(
   }[] = [];
 
   const errors: { address: string; error: string }[] = [];
-  const signer = await getSigner(mnemonic, keyPath);
 
   for (const a of data) {
     const addy =
